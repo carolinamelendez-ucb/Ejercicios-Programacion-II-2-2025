eb12ee97fb6802b9882f80d1302dffac95354aee
OnlyKxtt <163368008+OnlyKxtt@users.noreply.github.com>
2025-08-21 09:01:14 -0400

    KATRINA-CALAVI-ARREGLO-DINAMICO.cpp

--- KattCalavi.cpp
+++ KattCalavi.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Ingrese el tamaño del arreglo: ";
+    cin >> n;
+
+    int* arreglo = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Ingrese el elemento " << i + 1 << ": ";
+        cin >> arreglo[i];
+    }
+
+    cout << "\nElementos del arreglo: ";
+    for (int i = 0; i < n; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        suma += arreglo[i];
+    }
+
+    cout << "\nLa suma total es: " << suma << endl;
+
+    
+    delete[] arreglo;
+
+    return 0;
+}
