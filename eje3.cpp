803bc806477a92ad65915a88695b6b1e50734755
sasshuura <103129552+sasshuura@users.noreply.github.com>
2025-08-21 08:48:54 -0400

    test
    test

---
+++ eje3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    
+    cout << "Introduce el tamaño del arreglo: ";
+    cin >> n;
+
+  
+    int* arr = new int[n];
+
+   
+    cout << "Introduce " << n << " elementos:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Elemento " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+   
+    int maximo = arr[0];
+    int minimo = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+        if (arr[i] < minimo) {
+            minimo = arr[i];
+        }
+    }
+
+    cout << "El máximo es: " << maximo << endl;
+    cout << "El mínimo es: " << minimo << endl;
+
+    delete[] arr;
+
+    return 0;
+}
